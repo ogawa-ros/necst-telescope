76458b194f1ac796ef98f2222a97d15a2402c498
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-11-06 21:15:56 +0900

    add

--- scripts/refracted2apparent.py
+++ scripts/refracted2apparent.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 name = "refracted2apparent"
-kisa_path = "/home/exito/ros/src/necst-telescope/scripts/kisa.dat"
+kisa_path = "/home/exito/ros/src/necst-telescope/lib/kisa.dat"
 
 import math
 import rospy
