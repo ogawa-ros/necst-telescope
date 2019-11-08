e21d136c028373dd4b3c2689d17fe5775e0b39bb
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-11-08 18:29:40 +0900

    add opt-p

--- scripts/optical-pointing.py
+++ scripts/optical-pointing.py
@@ -127,8 +127,10 @@ class optical_pointing(object):
         try:
             for i in range(0, len(data)):
                 #print(float(data[i,1]), float(data[i,2]), data[i,3], data[i,4], data[i,5], data[i,6])
-                print("RA : " + str(data[i,1]), "DEC : " + str(data[i,2]))
                 print("star name : " + data[i,0])
+                print('No.%d (%d)' % (i+1, star_num))
+                print("RA : " + str(data[i,1]), "DEC : " + str(data[i,2]))
+
                 #self.ctrl.move_antenna_opt(px=data[i,3]/3600.*0, py=data[i,4]/3600*0, acc=3, x=data[i,1]*15., y=data[i,2], coord="J2000")
                 self.antenna.move_wcs(float(data[i,1])*15 ,float(data[i,2]))
                 self.antenna.tracking_check()
@@ -147,7 +149,7 @@ class optical_pointing(object):
                 late_el = self.antenna.get_el()
 
                 angle = [(pre_az+late_az)/2, (pre_el+late_el)/2]
-                print('No.%d (%d)' % (i+1, star_num))
+
                 print("angle " + str(angle))
                 print("captured image")
                 print("=========================================")
