532ebdc6abd9bb6d68c0eb477c8e3e1890339b9f
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-11-14 16:23:33 +0900

    add move_azel.py

---
+++ scripts/move_azel.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+For test of telescope driving
+"""
+import sys
+import ephem
+import time
+import numpy
+import math
+import os
+import pylab
+import datetime
+import matplotlib.pyplot as plt
+sys.path.append("/home/exito/necst-telescope/scripts")
+import telescope_controller
+sys.path.append("/home/exito/necst-core/scripts")
+import core_controller
+import rospy
+
+name = "move_azel"
+
+rospy.init_node(name)
+
+antenna = telescope_controller.antenna()
+
+az = antenna.get_az()
+el = antenna.get_el()
+print("current position : az="+str(az)+" el="+str(el))
+az_cmd = input("az = ")
+el_cmd = input("el = ")
+
+time.sleep(3)
+print("Moving az: "+str(az_cmd)+ ", el: "+str(el_cmd))
+antenna.move_azel(float(az_cmd),float(el_cmd))
+antenna.tracking_check()
+time.sleep(3)
