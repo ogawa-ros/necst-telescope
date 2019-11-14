ef67a8c470ce46f6555d942262239f095597c1f0
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-11-15 03:45:58 +0900

    add

---
+++ scripts/tracking_moon_test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+For test of telescope driving
+"""
+import sys
+import time
+import numpy
+import math
+import os
+import datetime
+sys.path.append("/home/exito/necst-telescope/scripts")
+import telescope_controller
+sys.path.append("/home/exito/necst-core/scripts")
+import core_controller
+import rospy
+
+name = "tracking_moon_test"
+
+rospy.init_node(name)
+
+logger = core_controller.logger()
+antenna = telescope_controller.antenna()
+
+date = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+file_name = name + '/' + date + '.necstdb'
+print(file_name)
+
+logger.start(file_name)
+
+time.sleep(3)
+print("Moving to moon")
+antenna.move_planet("moon")
+antenna.tracking_check()
+time.sleep(10)
+
+logger.stop()
