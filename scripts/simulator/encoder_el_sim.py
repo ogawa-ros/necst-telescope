cefaaa56d836ba2efc941531521a06191c14d13e
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-11-17 02:37:42 +0900

    add

--- scripts/simulator/encoder_el_sim.py
+++ scripts/simulator/encoder_el_sim.py
@@ -44,7 +44,7 @@ class encoder_el_sim(object):
 
                 enc_el_last = self.enc_el
 
-            time.sleep(0.01)
+            time.sleep(0.05)
             continue
 
     def start_thread(self):
