f8c3b6f40e9bbcd4f9515398faee2fc541e5022f
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-11-22 23:10:48 +0900

    add new launch

--- scripts/move_azel.py
+++ scripts/move_azel.py
@@ -15,12 +15,7 @@ name = "move_azel"
 
 rospy.init_node(name)
 
-logger = core_controller.logger()
 antenna = telescope_controller.antenna()
-
-date = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
-file_name = name + '/' + date + '.necstdb'
-print(file_name)
 
 az = antenna.get_az()
 el = antenna.get_el()
@@ -28,11 +23,8 @@ print("current position : az="+str(az)+" el="+str(el))
 az_cmd = input("az = ")
 el_cmd = input("el = ")
 
-logger.start(file_name)
-time.sleep(1)
 print("Moving az: "+str(az_cmd)+ ", el: "+str(el_cmd))
 #anntena.initilize()
 antenna.move_azel(float(az_cmd),float(el_cmd))
-#anntena.finalize()
 #antenna.tracking_check()
-logger.stop()
+#anntena.finalize()
