7ba68df9a1df56f4cbb5e890bea378aa01ce2728
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-12-22 19:44:58 +0900

    add

--- scripts/move_brightest_star.py
+++ scripts/move_brightest_star.py
@@ -26,7 +26,7 @@ class brightest_star(object):
 
     def __init__(self):
         rospy.init_node(name)
-        self.catalog_file = "/home/exito/ros/src/necst-1p85m/lib/bsc5.dat"
+        self.catalog_file = "/home/exito/ros/src/necst-1p85m2019/lib/bsc5.dat"
         self.camera = telescope_controller.camera()
         self.antenna = telescope_controller.antenna()
         self.logger = core_controller.logger()
