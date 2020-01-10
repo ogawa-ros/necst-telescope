7198c8324afee8fd16ced0cd673957af113d0098
takerumatsumoto <s_ta.matsumoto@p.s.osakafu-u.ac.jp>
2020-01-10 14:52:01 +0900

    add sun_scan

---
+++ experiment/sun_scan.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+import numpy
+import math
+import os
+import datetime
+
+sys.path.append("/home/exito/ros/src/necst-telescope/scripts")
+import telescope_controller
+sys.path.append("/home/exito/ros/src/necst-core/scripts")
+import core_controller
+sys.path.append("/home/exito/ros/src/necst-1p85m2019/scripts")
+import controller_1p85m2019
+
+import rospy
+
+planet = "sun"
+lx = 1 #deg
+ly = 1 #deg
+scan_t = 30
+
+name = "sun_scan"
+rospy.init_node(name)
+
+logger = core_controller.logger()
+antenna = telescope_controller.antenna()
+load = controller_1p85m2019.load()
+
+date = datetime.datetime.today().strftime('%Y%m%d_%H%M%S')
+file_name = name + '/' + date + '.necstdb'
+print(file_name)
+
+logger.start(file_name)
+
+load.move_hot()
+time.sleep(5)
+load.move_sky()
+time.sleep(5)
+
+antenna.move_raster_planet(planet,lx=lx,ly=0 ,scan_t=scan_t,l_unit="deg")
+time.sleep(1)
+antenna.move_raster_planet(planet,lx=0 ,ly=ly,scan_t=scan_t,l_unit="deg")
+
+logger.stop()
