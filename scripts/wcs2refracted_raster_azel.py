80b1693f3fa9d6671f7e158d222bd9fd734c8777
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2020-04-27 23:31:43 +0900

    add

--- scripts/wcs2refracted_raster_azel.py
+++ scripts/wcs2refracted_raster_azel.py
@@ -93,7 +93,9 @@ class wcs2refracted_raster_azel(object):
 
         #scan開始点に移動
         self.pub_stop_cmd.publish(True)
+
         time.sleep(0.3)
+        """
         for i in range(10):
             t = 0.1 * i
             altaz = self.convert_azel(x,y,dt=t)
@@ -104,6 +106,7 @@ class wcs2refracted_raster_azel(object):
             array.data = [obstime, az, alt]
             self.pub_real_azel.publish(array)
             time.sleep(0.001)
+        """
 
         print(0)
         while not self.tracking_check :
