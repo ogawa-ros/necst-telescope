c07ea87ed730df440850c56a417dbc49e98c2d07
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2020-04-27 22:20:41 +0900

    bug fix

--- scripts/wcs2refracted_raster_azel.py
+++ scripts/wcs2refracted_raster_azel.py
@@ -100,7 +100,7 @@ class wcs2refracted_raster_azel(object):
             array.data = [obstime, az, alt]
             self.pub_real_azel.publish(array)
 
-        while not self.tracking_check():
+        while not self.tracking_check() == True:
             altaz = self.convert_azel(x,y,dt=1)
             obstime = altaz.obstime.to_value("unix")
             az  = altaz.az.deg  + start_x/numpy.cos(math.radians(altaz.alt.deg))
